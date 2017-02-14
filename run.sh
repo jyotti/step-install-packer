6b97d4e0c8d09aeca5afab15478b0eb3672ab3e8
Atsushi Nakajyo <jyotti@gmail.com>
2017-02-14 14:50:49 +0900

    Change installation location

--- run.sh
+++ run.sh
@@ -1,60 +1,45 @@
 #!/bin/bash
+source ./tests/init.sh
 
-#set -x
+PACKER_SYMLINK_DIR="/usr/local/bin"
+PACKER_INSTALL_DIR="/usr/local/src"
 
-error_exit() {
-    local message=$1
-
-    echo "[FATAL] $message" 1>&2
-    exit 1
-}
-
-install_deps() {
-  local deps="0"
-  type unzip &> /dev/null || deps="1"
-  type wget &> /dev/null  || deps="1"
-  if [ ${deps} -eq "1" ]; then
-    echo "Install packages..."
-    apt-get update && apt-get install -y wget unzip
-  fi
-}
-
-download_packer() {
-  local install_path=$1
-  local version=$2
-
+check_packages() {
   set +e
-  install_deps
+  local pkgs=""
+  command -v curl &> /dev/null || pkgs+=" curl"
+  command -v unzip &> /dev/null || pkgs+=" unzip"
   set -e
-
-  if [ ! -f "${install_path}/packer_${version}_linux_amd64/packer" ]; then
-    echo "Install packer ..."
-    wget -qO ${install_path}/packer_${version}_linux_amd64.zip https://releases.hashicorp.com/packer/${version}/packer_${version}_linux_amd64.zip
-    unzip -qo "${install_path}/packer_${version}_linux_amd64.zip" -d "${install_path}/packer_${version}_linux_amd64"
-    rm -f ${install_path}/packer_${version}_linux_amd64.zip
+  if [ ! -z $pkgs ]; then
+    apt-get -y update && apt-get -y install $pkgs
   fi
-  ln -sf ${install_path}/packer_${version}_linux_amd64/packer ${install_path}/bin/packer
-  return 0
+}
+
+main () {
+  local version=$1
+  local install_dir=$2
+  if [ ! -f "${install_dir}/packer_${version}_linux_amd64/packer" ]; then
+    check_packages
+    info "Fetching Packer..."
+    curl -Lo "${install_dir}/packer_${version}_linux_amd64.zip" \
+            "https://releases.hashicorp.com/packer/${version}/packer_${version}_linux_amd64.zip"
+    info "Installing Packer..."
+    unzip -qo "${install_dir}/packer_${version}_linux_amd64.zip" \
+              -d "${install_dir}/packer_${version}_linux_amd64"
+    rm -f "${install_dir}/packer_${version}_linux_amd64.zip"
+  fi
+  ln -sf "${install_dir}/packer_${version}_linux_amd64/packer" "${PACKER_SYMLINK_DIR}/packer"
 }
 
 # check properties
-if [ ! -n "${WERCKER_INSTALL_PACKER_VERSION}" ]; then
-  error_exit 'Please specify the version property'
+if [[ ! -n "${WERCKER_INSTALL_PACKER_VERSION}" ]]; then
+  fail "Please specify the version property"
 fi
 
-# default
-# ${HOME}/github/jyotti/bin/packer
-# ${HOME}/github/jyotti/packer_x.x.x/packer
-#
-# cache
-# ${WERCKER_CACHE_DIR}/github/jyotti/bin/packer
-# ${WERCKER_CACHE_DIR}/github/jyotti/packer_x.x.x/packer
-INSTALL_PATH=""
-if [ -n "${WERCKER_INSTALL_PACKER_USE_CACHE}" -a "${WERCKER_INSTALL_PACKER_USE_CACHE}" == "true" ]; then
-  INSTALL_PATH=${WERCKER_CACHE_DIR}/github/jyotti
-else
-  INSTALL_PATH=${HOME}/github/jyotti
+if [[ "${WERCKER_INSTALL_PACKER_USE_CACHE}" == "true" ]]; then
+  PACKER_INSTALL_DIR="${WERCKER_CACHE_DIR}${PACKER_INSTALL_DIR}"
 fi
-mkdir -p ${INSTALL_PATH}/bin
-PATH=${PATH}:${INSTALL_PATH}/bin
-download_packer ${INSTALL_PATH} ${WERCKER_INSTALL_PACKER_VERSION}
+mkdir -p "${PACKER_SYMLINK_DIR}"
+mkdir -p "${PACKER_INSTALL_DIR}"
+
+main "${WERCKER_INSTALL_PACKER_VERSION}" "${PACKER_INSTALL_DIR}"
